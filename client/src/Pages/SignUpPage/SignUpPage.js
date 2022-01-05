041f1bbe19337b2ff1bebf42a803fc849ca6412d
jakepino <jakepino@yahoo.com>
2022-01-05 11:30:39 -0500

    fixing signup issue

--- client/src/Pages/SignUpPage/SignUpPage.js
+++ client/src/Pages/SignUpPage/SignUpPage.js
@@ -8,82 +8,98 @@ import Nav from '../../Components/Navigation/Navigation';
 import yoga from '../../images/yoga.jpg';
 
 function SignUpPage() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  const Fullname = useRef(null);
+  const Username = useRef(null);
+  const Email = useRef(null);
+  const Password = useRef(null);
 
-    const FullName = useRef(null);
-    const Username = useRef(null);
-    const Email = useRef(null);
-    const Password = useRef(null);
+  const submit = async () => {
+    setLoading(true);
+    await axios
+      .post(
+        'http://localhost:4000/users/signup',
+        {
+          fullname: Fullname.current.value,
+          username: Username.current.value,
+          email: Email.current.value,
+          password: Password.current.value,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        // navigate('/userdetails');
+        // setLoading(false);
+        window.location.replace('/userdetails');
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
 
-    const submit = async () => {
-        setLoading(true);
-        await axios.post('http://localhost:4000/users/signup', {
-                fullName: FullName.current.value,
-                username: Username.current.value,
-                email: Email.current.value,
-                password: Password.current.value,
-        }, { withCredentials: true })
-            .then((res) => {
-                console.log(res);
-                // navigate('/userdetails');
-                // setLoading(false);
-                window.location.replace('/userdetails');
-            })
-            .catch((err) => {
-                setLoading(false);
-                console.log(err);
-            })
-    }
+  return (
+    <>
+      <Nav />
+      <div style={{ backgroundImage: `url(${yoga})` }} className={style.main}>
+        {loading ? (
+          <div className={style.loader}></div>
+        ) : (
+          <div className={style.container}>
+            <form className={style.content}>
+              <h1>Sign Up</h1>
+              <br />
+              <br />
 
-    return (
-        <>
-            <Nav />
-            <div style={{backgroundImage: `url(${yoga})`}} className={style.main}>
-                { loading ? 
-                        <div className={style.loader}></div>
-                    :
-                    <div className={style.container}>
-                        <form className={style.content}> 
-                                <h1>Sign Up</h1>
-                                <br /><br />
+              <label htmlFor='nameS'>Full Name</label>
+              <br />
+              <input ref={Fullname} id='nameS' type='text' placeholder='Enter Your Name...' />
 
-                                <label htmlFor="nameS">Full Name</label>
-                                <br />
-                                <input ref={FullName} id='nameS' type="text" placeholder='Enter Your Name...' />
+              <br />
+              <br />
 
-                                <br /><br />
+              <label htmlFor='username'>Username</label>
+              <br />
+              <input ref={Username} id='username' type='text' placeholder='Enter Your Password...' />
 
-                                <label htmlFor="username">Username</label>
-                                <br />
-                                <input ref={Username} id='username' type="text" placeholder='Enter Your Password...' />
+              <br />
+              <br />
 
-                                <br /><br />
+              <label htmlFor='email'>Email</label>
+              <br />
+              <input ref={Email} id='email' type='text' placeholder='Enter Your Password...' />
 
-                                <label htmlFor="email">Email</label>
-                                <br />
-                                <input ref={Email} id='email' type="text" placeholder='Enter Your Password...' />
+              <br />
+              <br />
+              <br />
 
-                                <br /><br /><br />
-                                
-                                <label  htmlFor="passwordS">Password</label>
-                                <br />
-                                <input ref={Password} id='passwordS' type="password" placeholder='Enter Your Password...' />
+              <label htmlFor='passwordS'>Password</label>
+              <br />
+              <input ref={Password} id='passwordS' type='password' placeholder='Enter Your Password...' />
 
-                                <br /><br /><br />
-                                <button type='button' onClick={(e) => {
-                                    e.preventDefault();
-                                    submit()
-                                    console.log('clicked')
-                                }} className={style.btn}>Submit</button>
-                        </form>
-                    </div>
-                }
-            </div>
-        </>
-    )
+              <br />
+              <br />
+              <br />
+              <button
+                type='button'
+                onClick={(e) => {
+                  e.preventDefault();
+                  submit();
+                  console.log('clicked');
+                }}
+                className={style.btn}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default SignUpPage
+export default SignUpPage;
--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,4 @@
-const db = require("../db-models/db-models");
+const db = require('../db-models/db-models');
 
 const appControllers = {};
 
@@ -6,62 +6,60 @@ const appControllers = {};
 
 appControllers.login = async (req, res, next) => {
   const { username, password } = req.body;
-  console.log("this is username from controllers:", username);
-  const q = "SELECT * FROM users WHERE username=($1) AND password=($2)";
+  console.log('this is username from controllers:', username);
+  const q = 'SELECT * FROM users WHERE username=($1) AND password=($2)';
   await db.query(q, [username, password], (err, data) => {
     if (err) {
       return next(err);
     }
     if (data.rows.length > 0) {
-      console.log("user exist");
-      res.cookie("user", JSON.stringify(data.rows[0]), {
+      console.log('user exist');
+      res.cookie('user', JSON.stringify(data.rows[0]), {
         maxAge: 900000,
         httpOnly: false,
       });
-      res.locals.message = "successfully logged in";
+      res.locals.message = 'successfully logged in';
       return next();
     } else {
-      res.locals.error = "wrong username or password";
+      res.locals.error = 'wrong username or password';
       return next();
     }
   });
 };
 
 appControllers.signup = async (req, res, next) => {
-  const { fullName, username, password, email } = req.body;
-  // console.log("here is the username: ", username);
-  // console.log("type of username: ", typeof username);
+  const { fullname, username, password, email } = req.body;
 
   //create an empty object
   const validationErrors = {};
   // conditionals to check if entries are formatted correctly, if incorrect, add message to validationErrors object
   // if username is empty/less than x characters
   if (username.length === 0) {
-    validationErrors.username = "username is required";
+    validationErrors.username = 'username is required';
   } else if (username.length < 6) {
-    validationErrors.username = "Username must be min 6 characters";
+    validationErrors.username = 'Username must be min 6 characters';
   } else if (username.length > 20) {
-    validationErrors.username = "Username must be max 20 characters";
+    validationErrors.username = 'Username must be max 20 characters';
   }
 
   // if fullname is empty
 
   if (fullname.length === 0) {
-    validationErrors.fullname = "Please enter your full name.";
+    validationErrors.fullname = 'Please enter your full name.';
   }
   // if password is empty/less than x characters
   if (password.length === 0) {
-    validationErrors.password = "Please enter your password.";
+    validationErrors.password = 'Please enter your password.';
   } else if (password.length < 6) {
-    validationErrors.password = "Password must be min 6 characters";
+    validationErrors.password = 'Password must be min 6 characters';
   }
 
   if (email.length === 0) {
-    validationErrors.email = "Please enter an email address";
+    validationErrors.email = 'Please enter an email address';
   } else {
-    let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
     if (regex.test(email) === false) {
-      validationErrors.email = "Please enter a valid email address";
+      validationErrors.email = 'Please enter a valid email address';
     }
   }
   // if email is not correctly formatted as email address
@@ -71,7 +69,7 @@ appControllers.signup = async (req, res, next) => {
     return next();
   }
 
-  const q = "SELECT * FROM users WHERE username=($1) OR email=($2)";
+  const q = 'SELECT * FROM users WHERE username=($1) OR email=($2)';
 
   await db.query(q, [username, email], async (err, data) => {
     if (err) {
@@ -79,35 +77,30 @@ appControllers.signup = async (req, res, next) => {
     }
     console.log(err);
     const result = await data;
-    console.log("here is the response: ", result.rows.length);
+    console.log('here is the response: ', result.rows.length);
     if (result.rows.length > 0) {
-      res.locals.error =
-        "Account with this username/email already exists. Please try with different username/email";
+      res.locals.error = 'Account with this username/email already exists. Please try with different username/email';
       next();
     } else {
       await db.query(
-        "INSERT INTO users (username, fullname, password, email) VALUES (($1), ($2), ($3), ($4))",
+        'INSERT INTO users (username, fullname, password, email) VALUES (($1), ($2), ($3), ($4))',
         [username, fullname, password, email],
         async (err, data) => {
           if (err) {
             return next(err);
           }
-          console.log("hey i inserted the user");
+          console.log('hey i inserted the user');
 
-          await db.query(
-            "SELECT * FROM users WHERE username=($1)",
-            [username],
-            (err, data) => {
-              // console.log(data.rows[0]);
+          await db.query('SELECT * FROM users WHERE username=($1)', [username], (err, data) => {
+            // console.log(data.rows[0]);
 
-              res.cookie("user", JSON.stringify(data.rows[0]), {
-                maxAge: 90000,
-                httpOnly: false,
-              });
-              res.locals.message = "Successfully Signed Up!";
-              return next();
-            }
-          );
+            res.cookie('user', JSON.stringify(data.rows[0]), {
+              maxAge: 90000,
+              httpOnly: false,
+            });
+            res.locals.message = 'Successfully Signed Up!';
+            return next();
+          });
         }
       );
     }
